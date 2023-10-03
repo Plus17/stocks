edc5c70a2435fdd1171b46d201045c3c76767e29
Sergio <me@sergiovp.dev>
2023-10-03 13:15:36 -0600

    feat: adds Stooq HTTP client

---
+++ lib/stocks/http/stooq_client.ex
@@ -0,0 +1,67 @@
+defmodule Stocks.HTTP.StooqClient do
+  @moduledoc """
+  Implements the StooqSpec behavior to interact with the Stooq API over HTTP.
+
+  Valid options:
+
+    * `:format` - The format of the response. Defaults to `"json"`.
+  """
+  @behaviour Stocks.HTTP.StooqSpec
+
+  require Logger
+
+  alias Stocks.HTTP.StooqSpec
+
+  alias Stocks.Schemas.Tickers.Ticker
+
+  alias Stocks.Managers.Tickers.TickerManager
+
+  @base_url "https://stooq.com/q/l/"
+  @default_opts [format: "json"]
+
+  @impl true
+  @spec fetch_stock_data(StooqSpec.ticker(), Keyword.t() | nil) :: {:ok, Ticker.t()} | StooqSpec.req_error()
+  def fetch_stock_data(ticker, opts \\ @default_opts) do
+    format = Keyword.get(opts, :format, "json")
+    url = "#{@base_url}?s=#{ticker}.us&f=sd2t2ohlcv&h&e=#{format}"
+
+    case Req.get(url) do
+      {:ok, %Req.Response{status: 200, body: body}} ->
+        process_body(body)
+
+      {:ok, %Req.Response{status: status}} ->
+        Logger.error("[StooqAPI.fetch_stock_data] Unexpected status code: #{status}")
+        {:error, {:unexpected_status_code, status}}
+
+      {:error, %Mint.TransportError{reason: reason}} ->
+        Logger.error("[StooqAPI.fetch_stock_data] Transport error: #{inspect(reason)}")
+        {:error, {:transport_error, reason}}
+    end
+  end
+
+  @spec process_body(map) :: {:ok, map} | {:error, :invalid_ticker}
+  defp process_body(body) do
+    data =
+      body
+      |> Map.fetch!("symbols")
+      |> List.first()
+
+    case Map.get(data, "time") do
+      nil ->
+        Logger.warn(fn -> "[StooqAPI.process_body] Invalid ticker: #{inspect(data)}" end)
+        {:error, :invalid_ticker}
+
+      _req_time ->
+        {:ok, build_ticker(data)}
+    end
+  end
+
+  @spec build_ticker(map) :: Ticker.t()
+  defp build_ticker(data) do
+    data = Map.put(data, "requested_at", "#{data["date"]} #{data["time"]}")
+
+    %Ticker{}
+    |> TickerManager.change(data)
+    |> Ecto.Changeset.apply_changes()
+  end
+end
---
+++ lib/stocks/http/stooq_spec.ex
@@ -0,0 +1,31 @@
+defmodule Stocks.HTTP.StooqSpec do
+  @moduledoc """
+  Defines the contract for interacting with the Stooq API.
+  """
+
+  alias Stocks.Schemas.Tickers.Ticker
+
+  @typedoc """
+  The ticker for a stock. For example, "AAPL" for Apple.
+  """
+  @type ticker :: String.t()
+
+  @type req_error :: {:error, :invalid_ticker | {:transport_error, any} | {:unexpected_status_code, non_neg_integer}}
+
+  @doc """
+  Fetches stock data for the given ticker.
+
+  Returns `{:ok, data}` if the ticker is valid and the data was fetched successfully.
+  Returns `{:error, :invalid_ticker}` if the ticker is invalid.
+  Returns `{:error, {:transport_error, reason}}` if there was a transport error.
+  Returns `{:error, {:unexpected_status_code, status}}` if the response status code was not 200.
+
+  ## Examples
+
+      iex> fetch_stock_data("AAPL")
+      {:ok, %Ticker{}}
+      iex> fetch_stock_data("BAD")
+      {:error, :invalid_ticker}
+  """
+  @callback fetch_stock_data(ticker) :: {:ok, Ticker.t()} | {:error, any()}
+end
